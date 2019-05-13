7f6c6c7dcb4e96f1ceb699ff8ad72c79730c0b07
seuba <49449750+seuba@users.noreply.github.com>
2019-05-13 17:07:25 +0200

    Update whatsapp.php

--- whatsapp.php
+++ whatsapp.php
@@ -6,7 +6,7 @@
 
 $json4 = file_get_contents('php://input'); 
 $object = json_decode($json4, true);
-$missatge = $object['inArguments'][1]['message'];
+$missatge = $object['inArguments'][0]['message'];
 
 
 
