632abe0032816912161265adef3c4b4f76c0e4c6
seuba <49449750+seuba@users.noreply.github.com>
2019-05-15 14:18:13 +0200

    Update whatsapp.php

--- whatsapp.php
+++ whatsapp.php
@@ -23,8 +23,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $http_headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
-$object2 = json_decode($response, true);
-print_r($object2);
+
 
 
 //devolvemos el outArgument al config.json para utilizar en la split activity (true | false)
